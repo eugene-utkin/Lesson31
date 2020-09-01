44f0ecb0e6bcbb8e109f37b3f82772deaa171a45
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-01 19:17:54 +0500

    Product Model Added

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: "pizzashop.db"}
+
+class Product < ActiveRecord::Base
+
+end
 
 
 get '/' do
